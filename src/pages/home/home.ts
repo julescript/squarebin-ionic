ffe00ef8a3f04ad48781e2a4b1b61b8d9aaaa378
Julien <julien-hosry@outlook.com>
2019-05-01 17:56:41 +0300

    first

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {BarcodeScanner, BarcodeScannerOptions} from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,55 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  options: BarcodeScannerOptions;
 
+  constructor(private barcode: BarcodeScanner, public navCtrl: NavController) {
+
+  }
+
+  async scanBarcode(){
+    const results = await this.barcode.scan();
+    console.log(results);
+
+      if(results.text == "000001"){
+        var j = document.getElementById("jul").innerHTML;
+        var jj = parseInt(j) + 1;
+        document.getElementById("jul").innerHTML = jj.toString();
+      }
+
+      else if(results.text == "000002"){
+        var n = document.getElementById("nicolas").innerHTML;
+        var nn = parseInt(n) + 1;
+        document.getElementById("nicolas").innerHTML = nn.toString();
+      }
+
+      else if(results.text == "000003"){
+        var i = document.getElementById("issam").innerHTML;
+        var ii = parseInt(i) + 1;
+        document.getElementById("issam").innerHTML = ii.toString();
+      }
+
+      else if(results.text == "000004"){
+        var c = document.getElementById("clara").innerHTML;
+        var cc = parseInt(c) + 1;
+        document.getElementById("clara").innerHTML = cc.toString();
+      }
+
+      else if(results.text == "000005"){
+        var f = document.getElementById("jana").innerHTML;
+        var ff = parseInt(f) + 1;
+        document.getElementById("jana").innerHTML = ff.toString();
+      }
+
+      else if(results.text == "000006"){
+        var a = document.getElementById("jad").innerHTML;
+        var aa = parseInt(a) + 1;
+        document.getElementById("jad").innerHTML = aa.toString();
+      }
+
+      else{
+        alert("No Binner Found");
+      }
   }
 
 }
